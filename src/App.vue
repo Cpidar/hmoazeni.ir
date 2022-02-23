f3542857fe7cf20fe1b9beffd4c0234f04b11f42
Cpidar <sayeyesepidar@gmail.com>
2022-02-23 23:00:14 +0330

    add title, meta and sitemap

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,18 @@ import AOS from 'aos'
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'حسین موذنی | طراح و گرافیست',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'وبسایت شخصی حسین موذنی' },
+    { name: 'keywords', content: 'طراح گرافیک' },
+    { name: 'keywords', content: 'موشن گرافیک' },
+    { name: 'keywords', content: 'UI / UX Designer' },
+    { name: 'keywords', content: 'طراحی رابط کاربری' },
+    { name: 'keywords', content: 'هویت بصری' },
+    { name: 'keywords', content: 'طراحی لوگو' },
+    { name: 'keywords', content: 'فتوشاپ' },
+    { name: 'keywords', content: 'Adobe Photoshop' },
+    { name: 'author', content: 'حسین موذنی' },    
   ],
 })
 
